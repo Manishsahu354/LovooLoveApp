0c97fb824a622661ed851c87608d62e54df9e47f
manishkumar <manishsahu354@gmail.com>
2020-12-30 10:29:16 +0530

    Manish_First_commit

---
+++ app/src/main/java/com/example/lovoocloneapp/FourthActivityDOB.java
@@ -0,0 +1,14 @@
+package com.example.lovoocloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FourthActivityDOB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth_d_o_b);
+    }
+}
