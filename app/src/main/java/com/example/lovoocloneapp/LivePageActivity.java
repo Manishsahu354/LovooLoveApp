2572b7185e96cc8f24586ac5f21296b96c240828
Shaurabh2121 <2121aaravkaushal@gmail.com>
2020-12-31 20:55:00 +0530

    Shaurab seventh Commit

---
+++ app/src/main/java/com/example/lovoocloneapp/LivePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.lovoocloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_page);
+    }
+}
