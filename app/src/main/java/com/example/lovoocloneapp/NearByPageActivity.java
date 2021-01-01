9f55da8017d06a8e0d57dc40c59b6a8d99b590d3
Shaurabh2121 <2121aaravkaushal@gmail.com>
2021-01-01 09:43:11 +0530

    Shaurab eight Commit

---
+++ app/src/main/java/com/example/lovoocloneapp/NearByPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.lovoocloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearByPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_near_by_page);
+    }
+}
